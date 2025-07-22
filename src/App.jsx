2d619f1a9aca1bbbc23c79355fa0c63305c0147f
suryadeepkoduri <suryadeepkoduri@gmail.com>
2025-07-22 11:09:26 +0530

    fix: remove vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,8 +28,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-
-      <Analytics/>
     </>
   )
 }
